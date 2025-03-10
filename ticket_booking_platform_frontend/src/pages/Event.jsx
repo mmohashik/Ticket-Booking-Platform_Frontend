6a5ee6323494f41beca328403e1e810233b8e174
mmohashik <127235049+mmohashik@users.noreply.github.com>
2025-03-10 21:49:35 +0530

    Events page created

--- ticket_booking_platform_frontend/src/pages/Event.jsx
+++ ticket_booking_platform_frontend/src/pages/Event.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from '../components/NavBar'
+import Footer from '../components/Footer'
+import MiddleTextEvents from '../components/MiddleTextEvents'
+
+
 
 const Event = () => {
   return (
     <div>
       <Navbar/>
+      <MiddleTextEvents/>
+
+      <Footer/>
     </div>
   )
 }
